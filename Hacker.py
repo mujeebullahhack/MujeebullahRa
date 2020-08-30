12b8d9722a57831451b738f98266ea5b01c2adec
mujeebullahhack <70459741+mujeebullahhack@users.noreply.github.com>
2020-08-30 22:50:16 +0430

    Update and rename zeroboy.py to Hacker.py

--- Hacker.py
+++ Hacker.py
@@ -49,9 +49,9 @@ def jalan(z):
 ##### LOGO #####
 logo = """ -----------------------------•◈•
 (  __)\\ ____--------------_------------•◈•
-|__(~)    •||•THE - AAHIL -OFFICAL------•◈•
-|__\~~) •||•RANA - RAJPUT---------------•◈•
-|__(-----\  •◈•------BLACK-TIGER--------•◈•
+|__(~)    •||•MUJEEBULLAH - RAHMANI -OFFICAL------•◈•
+|__\~~) •||•MUJEEBULLAH - HACKER---------------•◈•
+|__(-----\  •◈•------AFGHAN BOXER--------•◈•
 |__~~~\ •◈•-----█-------⑦-------█------•◈•
 |__~~~\ •◈•-----█-------⑧-------█------•◈•
 |__~~~\ •◈•-----█-------⑥-------█------•◈•
@@ -87,8 +87,8 @@ print  """\033[1;91m=======================================
 \033[1;91m======================================="""
 print " \x1b[1;93m============================================================="
 
-CorrectUsername = "rana"
-CorrectPassword = "rana"
+CorrectUsername = "Mujeebullah"
+CorrectPassword = "Hacker"
 
 loop = 'true'
 while (loop == 'true'):
@@ -100,11 +100,14 @@ while (loop == 'true'):
             loop = 'false'
         else:
             print "Wrong Password"
-            os.system('xdg-open https://www.Youtube.com/UCsdJQbRf0xpvwaDu1rqgJuA')
+            os.system('xdg-open https://')
     else:
-        print "Wrong Username"
-        os.system('xdg-open https://www.Youtube.com/UCsdJQbRf0xpvwaDu1rqgJuA')
+or	
 
+        print "Wrong Username"
+        os.system('x
+		 ')
+https://youtu.be/hQJ-D5-x2rw
 def login():
 	os.system('clear')
 	try:
